ed2e0f2f4af3df5d29a4b5794dd3ecfeff8185f4
Kirubel Tesfaye <kirubeltesfaye519@gmail.com>
2026-03-16 02:26:05 +0300

    final auth edited

--- src/config/prisma.js
+++ src/config/prisma.js
@@ -1,5 +1,18 @@
-import { PrismaClient } from "../generated/prisma/client.js";
+import 'dotenv/config';
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import pg from 'pg';
 
-const prisma = new PrismaClient();
+const { Pool } = pg;
+
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+	throw new Error('DATABASE_URL is not set');
+}
+
+const adapter = new PrismaPg(new Pool({ connectionString }));
+
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
